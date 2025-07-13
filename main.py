e25f92a423d3700f3b9cd1a61fdea6a626a75740
Shelby <79418372+shelbyam27@users.noreply.github.com>
2025-07-13 14:05:09 +0700

    Update main.py

--- main.py
+++ main.py
@@ -223,4 +223,4 @@ def get_tiktok_info(username: str):
     )
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
